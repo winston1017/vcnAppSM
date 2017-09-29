6624af1e282403b168ae3b2d501171fb6cf96c79
winston1017 <winston1017@gmail.com>
2017-09-29 05:13:51 -0700

    Compact code

--- app/src/main/java/com/example/user/vcnapp/MainActivity.java
+++ app/src/main/java/com/example/user/vcnapp/MainActivity.java
@@ -11,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String url = "http://www.streetmessenger.io/";
-        Intent i = new Intent(Intent.ACTION_VIEW);
-        i.setData(Uri.parse(url));
-        startActivity(i);
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
     }
 }
